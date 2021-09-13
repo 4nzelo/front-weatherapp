65d09bc3ecd8f57e41e9614a78ed96c16ecce862
unknown <anderson.ramirez.icesi@gmail.com>
2021-09-13 10:57:07 -0500

    update app.css

--- front-weatherapp/src/WeatherCard.js
+++ front-weatherapp/src/WeatherCard.js
@@ -13,9 +13,9 @@ export default class WeatherCard extends Component {
                 <div className = "card-title2"> {this.props.date} </div> 
                 <div className = "card-title3"> {this.props.temp}°</div> 
                 <CListGroup flush>
-                    <CListGroupItem> <CImage src="/images/temp.png" width={30} height={30}   />   Temperatura { this.props.temp }  </CListGroupItem>
-                    <CListGroupItem> <CImage src="/images/humidity.png" width={30} height={30} />  Humedad  { this.props.humidity }% </CListGroupItem>
-                    <CListGroupItem> <CImage src="/images/wind.png" width={30} height={30} />  Velocidad Viento  { this.props.speedWind } m/s  </CListGroupItem>
+                    <CListGroupItem> <CImage src="/images/temp.png" width={30} height={30}   />   Temperatura <span className = "cardValue"> { this.props.temp } </span>  </CListGroupItem>
+                    <CListGroupItem> <CImage src="/images/humidity.png" width={30} height={30} />  Humedad  <span className = "cardValue"> { this.props.humidity }% </span> </CListGroupItem>
+                    <CListGroupItem> <CImage src="/images/wind.png" width={30} height={30} />  Velocidad Viento  <span className = "cardValue"> { this.props.speedWind } m/s </span> </CListGroupItem>
                 </CListGroup>
             </CCard>
             </div>
